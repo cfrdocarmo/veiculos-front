0783435bbe936efd3c5faba1d814d3fd5613df2f
cfrdocarmo <carlos.felipe.ramos67@aluno.ifce.edu.br>
2022-03-18 07:33:06 -0300

    Conecção do Back(SpringBoot) com Front(Angular)

--- src/app/services/VeiculoService/veiculo.service.ts
+++ src/app/services/VeiculoService/veiculo.service.ts
@@ -14,47 +14,6 @@ export class VeiculoService {
   constructor( private httpClient: HttpClient ) { }
 
   getAll() {
-    return this.httpClient.get<Veiculo[]>(this.API);/* [{
-      "id": 1,
-      "marca": "HONDA",
-      "veiculo": "jetta",
-      "ano": 2015,
-      "descricao": "balblablablblal"
-    },
-    {
-      "id": 2,
-      "marca": "FIAT",
-      "veiculo": "jetta",
-      "ano": 2020,
-      "descricao": "balblablablblal"
-    },
-    {
-      "id": 3,
-      "marca": "KIA",
-      "veiculo": "jetta",
-      "ano": 2008,
-      "descricao": "balblablablblal"
-    },
-    {
-      "id": 4,
-      "marca": "TOYOTA",
-      "veiculo": "jetta",
-      "ano": 1999,
-      "descricao": "balblablablblal"
-    },
-    {
-      "id": 5,
-      "marca": "CHEVROLET",
-      "veiculo": "jetta",
-      "ano": 2003,
-      "descricao": "balblablablblal"
-    },
-    {
-      "id": 6,
-      "marca": "RENAULT",
-      "veiculo": "jetta",
-      "ano": 2022,
-      "descricao": "balblablablblal"
-    }] */
+    return this.httpClient.get<Veiculo[]>(this.API);
   }
 }
